8311225196548d47955056695f01a3338959cfac
Code Sandbox <hello@codesandbox.io>
2022-11-01 06:42:28 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.b41b320d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{3:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),c=n(2),s=n.n(c),i=a.a.createElement("div",{},"hi....");s.a.render(i,document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.b41b320d.chunk.js.map
